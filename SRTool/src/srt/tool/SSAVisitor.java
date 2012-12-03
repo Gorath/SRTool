9e076e67bf0ec94cf6cd130a070820d23e8e00fc
Abhijit Chandgadkar <ac4309@doc.ic.ac.uk>
2012-12-03 22:57:06 +0000

    Shit wont work now but will fix it

--- SRTool/src/srt/tool/SSAVisitor.java
+++ SRTool/src/srt/tool/SSAVisitor.java
@@ -9,33 +9,41 @@ import srt.ast.DeclRef;
 import srt.ast.visitor.impl.DefaultVisitor;
 
 public class SSAVisitor extends DefaultVisitor {
-	private Map<String, Integer> index = new HashMap<String, Integer>();
+    private Map<String, Integer> index = new HashMap<String, Integer>();
 
-	public SSAVisitor() {
-		super(true);
-	}
-	
-	private String getSSAName(String name) {
-		return null;
-	}
-	
-	private void incrementSSAIndex(String name) {
-		
-	}
-	
-	@Override
-	public Object visit(Decl decl) {
-		return super.visit(decl);
-	}
-	
-	@Override
-	public Object visit(DeclRef declRef) {
-		return super.visit(declRef);
-	}
-	
-	@Override
-	public Object visit(AssignStmt assignment) {
-		return super.visit(assignment);
-	}
+    public SSAVisitor() {
+        super(true);
+    }
 
+    private String getSSAName(String name) {
+        int i = index.get(name);
+        return name + "$" + i;
+    }
+
+    private void incrementSSAIndex(String name) {
+        if (index.get(name) == null){
+            index.put(name, 0);
+        } else {
+            int i = index.get(name) + 1;
+            index.put(name, i);
+        }
+    }
+
+    @Override
+    public Object visit(Decl decl) {
+        return super.visit(decl);
+    }
+
+    @Override
+    public Object visit(DeclRef declRef) {
+        return super.visit(new DeclRef(getSSAName(declRef.getName())));
+    }
+
+    @Override
+    public Object visit(AssignStmt assignment) {
+        String name = assignment.getLhs().getName();
+        incrementSSAIndex(name);
+        String ssaName = getSSAName(name);
+        return super.visit(new AssignStmt(new DeclRef(ssaName), assignment.getRhs()));
+    }
 }
---
+++ SRTool/test/SSA/SSA_good.sc
@@ -0,0 +1,8 @@
+
+void main()
+{
+    int i;
+    i = 4;
+
+    assert(i==4);
+}
