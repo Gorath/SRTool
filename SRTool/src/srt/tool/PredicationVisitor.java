9556efe6ac0da15851403be5b3f49f20c7bf4516
deewar <osama.a.javed@gmail.com>
2012-12-05 17:00:35 +0000

    abz can continue from here

--- SRTool/src/srt/tool/PredicationVisitor.java
+++ SRTool/src/srt/tool/PredicationVisitor.java
@@ -3,28 +3,37 @@ package srt.tool;
 import srt.ast.*;
 import srt.ast.visitor.impl.DefaultVisitor;
 
+import java.util.Random;
+import java.util.Stack;
+
 public class PredicationVisitor extends DefaultVisitor {
+    private Expr GLOBAL_PRED =  new IntLiteral(1);
+    private Stack<Expr> stack = new Stack<Expr>();
+    private final String baseName = "$PRED";
+    private int baseInt = 0;
 
 	public PredicationVisitor() {
-		super(true);
+        super(true);
+        stack.push(GLOBAL_PRED);
 	}
-	
+
+    private String getNextName(){
+        return  baseName + (baseInt++);
+    }
+
 	@Override
-	public Object visit(IfStmt ifStmt) {
-        Expr p = ifStmt.getCondition();
+	public Object visit(IfStmt ifStmt ) {
+        Expr e = ifStmt.getCondition();
         Stmt q = ifStmt.getThenStmt();
         Stmt r = ifStmt.getElseStmt();
 
-
-
-        //TernaryExpr te = new TernaryExpr(p,q,null,);
-
-		return super.visit(ifStmt);
+        return new TernaryExpr(e,null,null,ifStmt);
 	}
 
 	@Override
 	public Object visit(AssertStmt assertStmt) {
-		return super.visit(assertStmt);
+
+        return super.visit(assertStmt);
 	}
 
 	@Override
@@ -34,12 +43,28 @@ public class PredicationVisitor extends DefaultVisitor {
 
 	@Override
 	public Object visit(AssumeStmt assumeStmt) {
-		return super.visit(assumeStmt);
+        Expr condition = assumeStmt.getCondition();
+        DeclRef A = new DeclRef( getNextName());
+        Decl declareStmnt = new Decl(A.getName(),"int");
+        BinaryExpr binaryExpr = new BinaryExpr(BinaryExpr.LOR,
+                                new UnaryExpr(UnaryExpr.LNOT,gAndP()),
+                                condition);
+        AssignStmt assign = new AssignStmt(A ,binaryExpr);
+        GLOBAL_PRED = new BinaryExpr(BinaryExpr.LAND, GLOBAL_PRED, A);
+        return new BlockStmt(new Stmt[]{declareStmnt,assign},assumeStmt);
 	}
 
 	@Override
 	public Object visit(HavocStmt havocStmt) {
-		return super.visit(havocStmt);
+         DeclRef x = havocStmt.getVariable();
+         DeclRef h = new DeclRef( getNextName());
+         Decl declareStmnt = new Decl(h.getName(),"int");
+         TernaryExpr te = new TernaryExpr(gAndP(),h,x);
+         AssignStmt assign = new AssignStmt(x ,te);
+         return new BlockStmt(new Stmt[]{declareStmnt,assign},havocStmt);
 	}
 
+    private BinaryExpr gAndP(){
+         return new BinaryExpr(BinaryExpr.LAND,GLOBAL_PRED,stack.peek());
+    }
 }
---
+++ SRTool/test/predication/assume_bad.sc
@@ -0,0 +1,10 @@
+
+void main(int a)
+{
+	a = 2;
+
+	assume(a==10);
+
+	assert(a != 2);
+
+}
