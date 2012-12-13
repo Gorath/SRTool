c85239d4c0755e41f554b12daf0e7fdffb60d5cd
Gorath <generalredeye@gmail.com>
2012-12-13 16:52:02 +0000

    Loop unwinding working - still need to do invariants

--- SRTool/src/srt/tool/LoopUnwinderVisitor.java
+++ SRTool/src/srt/tool/LoopUnwinderVisitor.java
@@ -1,6 +1,6 @@
 package srt.tool;
 
-import srt.ast.WhileStmt;
+import srt.ast.*;
 import srt.ast.visitor.impl.DefaultVisitor;
 
 public class LoopUnwinderVisitor extends DefaultVisitor {
@@ -17,7 +17,38 @@ public class LoopUnwinderVisitor extends DefaultVisitor {
 
 	@Override
 	public Object visit(WhileStmt whileStmt) {
-		return super.visit(whileStmt);
+
+
+        AssertStmt loopAssert = new AssertStmt(new UnaryExpr(UnaryExpr.LNOT,whileStmt.getCondition()),whileStmt);
+        AssumeStmt loopAssume = new AssumeStmt(new UnaryExpr(UnaryExpr.LNOT,whileStmt.getCondition()));
+
+        Stmt[] statementArray;
+        if (unwindingAssertions) {
+            statementArray = new Stmt[2];
+            statementArray[0] = loopAssert;
+            statementArray[1] = loopAssume;
+        } else {
+            statementArray = new Stmt[1];
+            statementArray[0] = loopAssume;
+        }
+
+        Stmt blockStatementBase = new BlockStmt(statementArray);
+        Stmt whileBlock = whileStmt.getBody();
+
+        for (int i = 0; i < whileStmt.getBound().getValue(); i++) {      // Check if getbound is null
+
+            blockStatementBase = new BlockStmt(new Stmt[]{whileBlock,blockStatementBase});
+
+            blockStatementBase = new IfStmt(whileStmt.getCondition(), blockStatementBase , new EmptyStmt());
+
+        }
+
+        Stmt w2 = new BlockStmt(new Stmt[]{blockStatementBase},whileBlock);
+
+        return super.visit(w2);
+
+        //return super.visit(whileStmt);
+
 	}
 
 }
